83f4770f371f1ee9e0886f63352271ad2a0dc609
Karen Fernanda Tarazona Jaimes <k.tarazonaj@uniandes.edu.co>
2025-09-14 10:12:47 -0500

    feat: Actualiza la configuración de SQLALCHEMY_DATABASE_URI para usar DB_URL

--- src/alpespartners/config/db.py
+++ src/alpespartners/config/db.py
@@ -2,6 +2,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
 import os, importlib
 
+
 db = SQLAlchemy()
 
 MODEL_MODULES = [   
@@ -11,7 +12,12 @@ MODEL_MODULES = [
 ]
 
 def init_db(app: Flask):
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'postgresql+psycopg2://postgres:10100101@localhost:5432/alpespartner')
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv(
+        'DB_URL',
+        'postgresql+psycopg2://partner:partner@postgres:5432/alpespartner'
+        #'postgresql+psycopg2://partner:partner@127.0.0.1:5432/alpespartner'
+    )
+    # Opcional: evita warnings y corta conexiones zombie
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {'pool_pre_ping': True}
     db.init_app(app)
